5fab145a30de8fdce2b6efdd4c62b57fcb2e2a7f
nadya <tutu87238@gmail.com>
2024-04-24 21:51:18 +0700

    java

--- src/test/java/example/ExampleControllerTest.java
+++ src/test/java/example/ExampleControllerTest.java
@@ -3,6 +3,7 @@ package example;
 import example.person.Person;
 import example.person.PersonRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -15,36 +16,40 @@ import static org.mockito.BDDMockito.given;
 
 class ExampleControllerTest {
 
-    private ExampleController subject;
     @Mock
     private PersonRepository personRepository;
+    @Mock
+    private ExampleController exampleController;
 
     @BeforeEach
     public void setUp() throws Exception {
         MockitoAnnotations.openMocks(this);
-        subject = new ExampleController(personRepository);
+        exampleController = new ExampleController(personRepository);
     }
 
     @Test
+    @DisplayName("Возврашение фразы 'Здравствуй Мир!'")
     public void shouldReturnHelloWorld() {
-        assertEquals("Здравствуй Мир!", subject.hello());
+        assertEquals("Здравствуй Мир!", exampleController.hello());
     }
 
     @Test
+    @DisplayName("Возвращение полного имени человека")
     public void shouldReturnFullNameOfAPerson() throws Exception {
         Person peter = new Person("Иван", "Иванов");
         given(personRepository.findByLastName("Иванов")).willReturn(Optional.of(peter));
 
-        var greeting = subject.hello("Иванов");
+        var greeting = exampleController.hello("Иванов");
 
         assertEquals("Здравствуй, Иван Иванов!", greeting);
     }
 
     @Test
+    @DisplayName("Сообщение, что человек неизвестен")
     public void shouldTellIfPersonIsUnknown() throws Exception {
         given(personRepository.findByLastName(anyString())).willReturn(Optional.empty());
 
-        var greeting = subject.hello("Иванов");
+        var greeting = exampleController.hello("Иванов");
 
         assertEquals("Не знаю никого по фамилии 'Иванов'", greeting);
     }
